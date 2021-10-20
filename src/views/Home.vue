852b9d3d392320b85f9f4093e780d61208dccbe7
Aliyama <alwiarif72@yahoo.com>
2021-10-20 09:00:20 +0700

    First Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <Header />
+    <Hero />
+    <Subhero />
+    <InstaPhotos />
+    <PartnerLogos />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+import Hero from '@/components/Hero.vue'
+import Subhero from '@/components/Subhero.vue'
+import InstaPhotos from '@/components/InstaPhotos.vue'
+import PartnerLogos from '@/components/PartnerLogos.vue'
+import Footer from '@/components/Footer.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Hero,
+    Subhero,
+    InstaPhotos,
+    PartnerLogos,
+    Footer
+  }
+}
+</script>
